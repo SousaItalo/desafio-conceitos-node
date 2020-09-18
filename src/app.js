d4e5061e6de87fe5c8784976c31956c2de8e3709
Italo Sousa <italops94@gmail.com>
2020-09-18 12:17:20 -0300

    store, index, update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,15 +10,38 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { body: { url, title, techs } } = request;
+
+  const repository = { id: uuid(), url, title, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {
+    body: { url, title, techs },
+    params: { id }
+  } = request;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response
+      .status(404)
+      .json({ error: "Repository not found" });
+  }
+
+  const updatedRepo = { id, url, title, techs };
+  repositories[repositoryIndex] = updatedRepo;
+  
+  return response.json(updatedRepo);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
